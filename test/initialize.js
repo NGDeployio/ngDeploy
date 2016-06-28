067589e3f489270f418e70ecfc0f4718ea6e84ba
Marek <marek.bejda@gmail.com>
2016-06-28 13:40:22 -0400

    updated tests and styling.

--- test/initialize.js
+++ test/initialize.js
@@ -1,63 +1,71 @@
-var ngdeploy= require('../lib/cli.js');
+var ngdeploy = require('../lib/cli.js');
 var fs = require('fs');
 var chai = require('chai');
 
 var assert = chai.assert;
-var should=chai.should();
+var should = chai.should();
 
-var test = {'name':'worldtest','version':'0.1.1','dist':'./dist','path':'','endpoint':'https://api.ngdeploy.com', 'id':11};
+var test = {
+    name: 'testapp',
+    version: '0.1.1',
+    dist: './dist',
+    ngDeployUrl: 'testapp',
+    path: '',
+    endpoint: 'https://api.ngdeploy.com',
+    accountToken: '123123123',
+    id: 11, };
 
-describe('ngdeploy', function() {
-        describe('.init()', function() {
-            before(function() {
+describe('ngdeploy', function () {
+        describe('.init()', function () {
+            before(function () {
                 if (fs.existsSync('.ngdeploy')) {
-                    fs.unlinkSync('.ngdeploy')
+                  fs.unlinkSync('.ngdeploy');
                 }
-            });
+              });
 
-            it('should not create an empty .ngdeploy file', function(){
+            it('should not create an empty .ngdeploy file', function () {
                 ngdeploy.init();
-                assert.equal(fs.existsSync('.ngdeploy'),false);
-            });
-    });
+                assert.equal(fs.existsSync('.ngdeploy'), false);
+              });
+          });
 
-    describe('.init(appname,./dist)', function() {
-        before(function(){
-            if(fs.existsSync('.ngdeploy')){
-                fs.unlinkSync('.ngdeploy')
-            }
-        });
-        it('should create an .ngdeploy file', function(){
-            ngdeploy.update('name', 'HelloWorld');
-            ngdeploy.update('ngDeployUrl','HelloWorld');
-            ngdeploy.update('dist', './app');
-            ngdeploy.update('id',1);
-            ngdeploy.createConfiguration({'id':'123'});
-            assert.equal(fs.existsSync('.ngdeploy'),true);
+        describe('.init(appname,./dist)', function () {
+            before(function () {
+                if (fs.existsSync('.ngdeploy')) {
+                  fs.unlinkSync('.ngdeploy');
+                }
 
-            var testJson=JSON.parse(fs.readFileSync('.ngdeploy'));
-            assert.equal(testJson.name,'HelloWorld');
-            assert.equal(testJson.dist,'./app');
-            assert.equal(testJson.id,'123');
+                console.log('before');
+              });
 
-        });
+            it('should create an .ngdeploy file', function () {
 
-        after(function(){
-            if(fs.existsSync('.ngdeploy')){
-                fs.unlinkSync('.ngdeploy')
-            }
-        });
-    });
+                ngdeploy.createConfiguration(test);
+                assert.equal(fs.existsSync('.ngdeploy'), true);
 
-    describe('.clean()', function() {
-        before(function(){
-            ngdeploy.createConfiguration(JSON.stringify(test));
-        });
+                var testJson = JSON.parse(fs.readFileSync('.ngdeploy'));
+                assert.equal(testJson.name, 'testapp');
+                assert.equal(testJson.dist, './dist');
+                assert.equal(testJson.id, 11);
 
-        it('should remove the .ngdeploy file', function(){
-            ngdeploy.clean();
-            assert.equal(fs.existsSync('.ngdeploy'),false);
-        });
-    });
+              });
 
-});
+            after(function () {
+                if (fs.existsSync('.ngdeploy')) {
+                  fs.unlinkSync('.ngdeploy');
+                }
+              });
+          });
+
+        describe('.clean()', function () {
+            before(function () {
+                ngdeploy.createConfiguration(test);
+              });
+
+            it('should remove the .ngdeploy file', function () {
+                ngdeploy.clean();
+                assert.equal(fs.existsSync('.ngdeploy'), false);
+              });
+          });
+
+      });
