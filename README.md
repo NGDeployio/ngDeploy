6064c1222c8497a23aeea2f2f9f2a7fd6541b689
Marek <marek5050@users.noreply.github.com>
2016-07-08 11:51:41 +0200

    Fixed up documentation and tests (#14)
    * Updating TravisCI
    
    * Pull dev if you need to
    
    * initial package README
    
    * updated README
    
    * moved line
    
    * initial steps
    
    * Added format and examples
    
    * saving changes
    
    * Checking coveralls
    
    * fixed up tests
    
    * simplified function
    
    * simplified func
    
    * Fixed codehound issues

--- README.md
+++ README.md
@@ -1,17 +1,87 @@
-[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
-[![TravisCI Status](https://travis-ci.org/NGDeployio/ngDeploy.svg)](https://travis-ci.org/NGDeployio)
-[![bitHound Overall Score](https://www.bithound.io/github/NGDeployio/ngDeploy/badges/score.svg)](https://www.bithound.io/github/NGDeployio/ngDeploy)
+# NGDeploy CLI [![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com) [![TravisCI Status](https://travis-ci.org/NGDeployio/ngDeploy.svg)](https://travis-ci.org/NGDeployio) [![bitHound Overall Score](https://www.bithound.io/github/NGDeployio/ngDeploy/badges/score.svg)](https://www.bithound.io/github/NGDeployio/ngDeploy)
 
-*NGDeploy CLI*
-
-> Angular and Static web hosting CLI tool.
+> Angular and single page application hosting CLI tool.
 
 Install this globally and you'll have access to the `ngdeploy` command anywhere on your system.
 
 ```shell
 [sudo] npm install -g ngdeploy
 ```
+**Note:** The job of the NGDeploy CLI is to push and build single page applications. It's preferred to install globally, but local installations will work too. [please read this](http://nodejs.org/en/blog/npm/npm-1-0-global-vs-local-installation).
 
-**Note:** The job of the NGDeploy CLI is to upload and build single page applications. It's prefered to install globally, but local installations will work too. [please read this](http://nodejs.org/en/blog/npm/npm-1-0-global-vs-local-installation).
+## Usage
+```shell
+$ ngdeploy --help
+1.    $ ngdeploy set -g -a <AccountToken>
+2.    $ ngdeploy create <appName>
+3.    $ ngdeploy init <appName> <distDir>
+4.    $ ngdeploy push
+Example: 
+1.    $ ngdeploy set -g -a 123456789
+2.    $ ngdeploy create helloApp
+3.    $ ngdeploy init helloApp ./dist
+4.    $ ngdeploy push
+```
 
-<!--See the [Getting Started](http://ngdeploy.com/getting-started) guide for more information.-->
+## First steps
+
+### Install NGDeploy
+
+**Example**
+```shell
+npm install -g ngdeploy 
+```
+
+### Login
+[Generate an account token here.](http://ngdeploy.com/registerUser) We'll set the account token globally using the following command.
+**Format**
+> ngdeploy login
+
+**Example**
+```shell
+ngdeploy login 
+$ prompt: Open this link in a browser to register: https://ngdeploy.com/express
+$ prompt: Account Token to use, by default global ~/.ngdeploy:  **************
+```
+
+### Create
+Creating your first application is as easy as:
+
+**Format**  
+> ngdeploy create \<Application Name> 
+
+**Example**  
+```shell
+ngdeploy create HelloWorld
+```
+> Please note application names must be unique.
+
+### Initialize
+Now we can initialize the .ngdeploy configuration file using:
+**Format**
+> ngdeploy init \<Application Name> \<Distribution directory>
+
+**Example**
+```shell
+ngdeploy init HelloWorld .  
+```
+
+### Push
+> ngdeploy push
+
+**Example**  
+```shell
+ngdeploy push
+```
+
+### Environment
+Environmental variables can be injected into a SPA using the setEnv command. Upon a 
+user request this embeds a javascript file with the given environmental variables. 
+
+**Format** 
+> ngdeploy setEnv \<Application Id> \<Stage> \<key:val>
+
+**Example**
+```shell
+ngdeploy setEnv 1 production key:val
+```
--- test/initialize.js
+++ test/initialize.js
@@ -1,18 +1,8 @@
 var ngdeploy = require('../lib/cli.js');
 var fs = require('fs');
 var chai = require('chai');
-
+var test = require('./fixtures/ngdeploy.json');
 var assert = chai.assert;
-
-var test = {
-    name: 'testapp',
-    version: '0.1.1',
-    dist: './dist',
-    ngDeployUrl: 'testapp',
-    path: '',
-    endpoint: 'https://api.ngdeploy.com',
-    accountToken: '123123123',
-    id: 11, };
 
 function clearConfig(){
     if (fs.existsSync('.ngdeploy')) {
@@ -21,13 +11,32 @@ function clearConfig(){
 }
 
 describe('ngdeploy', function () {
-        describe('.init()', function () {
+          describe('.login()', function(){
+            before(function(){
+                clearConfig();
+            });
+            it('sets the global access key', function(){
+                ngdeploy.login({global:1, test:1, accountToken:"global token"});
+                ngdeploy.readngdeploy();
+                assert.equal(ngdeploy.get("accountToken"), "global token");
+            });
+
+            it('sets the local access key', function(){
+                ngdeploy.login({local:1, test:1, accountToken:"local token"});
+                ngdeploy.readngdeploy();
+                assert.equal(ngdeploy.get("accountToken"), "local token");
+            });
+
+          });
+         describe('.init()', function () {
             before(function () {
                 clearConfig();
               });
 
             it('should not create an empty .ngdeploy file', function () {
-                ngdeploy.init();
+                try{
+                    ngdeploy.init();
+                }catch(e){}
                 assert.equal(fs.existsSync('.ngdeploy'), false);
               });
           });
@@ -38,11 +47,17 @@ describe('ngdeploy', function () {
              });
 
             it('should create an .ngdeploy file', function () {
+                var testJson;
 
-                ngdeploy.createConfiguration(test);
+                try{
+                    ngdeploy.createConfiguration(test);
+                }catch(e){}
+
                 assert.equal(fs.existsSync('.ngdeploy'), true);
 
-                var testJson = JSON.parse(fs.readFileSync('.ngdeploy'));
+                try {
+                     testJson = JSON.parse(fs.readFileSync('.ngdeploy'));
+                }catch(e){}
                 assert.equal(testJson.name, 'testapp');
                 assert.equal(testJson.dist, './dist');
                 assert.equal(testJson.id, 11);
@@ -56,13 +71,62 @@ describe('ngdeploy', function () {
 
         describe('.clean()', function () {
             before(function () {
-                ngdeploy.createConfiguration(test);
+                try{
+                    ngdeploy.createConfiguration(test);
+                }catch(e){}
               });
 
-            it('should remove the .ngdeploy file', function () {
+            it('(default) should remove the .ngdeploy file', function () {
                 ngdeploy.clean();
                 assert.equal(fs.existsSync('.ngdeploy'), false);
               });
+
+            it(' -g should remove the ~/.ngdeploy file', function () {
+                ngdeploy.setAccountToken("test account",1);
+                ngdeploy.clean({global:1});
+                assert.equal(fs.existsSync('.ngdeploy'), false);
+            });
+
+            it(' -l should remove the .ngdeploy file', function(){
+                    ngdeploy.setAccountToken("test account",0);
+                    ngdeploy.clean({local:1});
+                    assert.equal(fs.existsSync('.ngdeploy'), false);
+            });
+            it(' -l -g should remove both .ngdeploy files', function(){
+                ngdeploy.setAccountToken("local account",0);
+                ngdeploy.setAccountToken("global account",1);
+                ngdeploy.clean({local:1, global:1});
+                assert.equal(fs.existsSync('.ngdeploy'), false);
+                assert.equal(fs.existsSync( ngdeploy.getUserHome() + '/.ngdeploy'), false);
+            });
           });
 
-      });
+
+    describe('init with local and global accessToken', function () {
+        before(function () {
+            try{
+            }catch(e){}
+        });
+
+        it('should use global by default', function () {
+            ngdeploy.createConfiguration(test);
+            ngdeploy.setAccountToken("global token", 1);
+            ngdeploy.readngdeploy();
+            assert.equal(ngdeploy.get("accountToken"), "global token");
+        });
+
+        it('should use the local if both present', function () {
+            ngdeploy.createConfiguration(test);
+            ngdeploy.setAccountToken("global token", 1);
+            ngdeploy.setAccountToken("local token", 0);
+            ngdeploy.readngdeploy();
+            assert.equal(ngdeploy.get("accountToken"), "local token");
+        });
+
+        after(function () {
+            clearConfig();
+        });
+
+    });
+
+});
