dd9368e9be7a9aa7c91458e08e4ad1217d54caea
Marek <marek.bejda@gmail.com>
2016-07-19 16:03:14 -0400

    Moved config management into config.js

--- test/initialize.js
+++ test/initialize.js
@@ -13,120 +13,123 @@ var expect = chai.expect;
 
 var config = require('../lib/config');
 
-function clearConfig(){
-    ngdeploy.clean({local:1,global:1});
+function clearConfig() {
+    ngdeploy.clean({local: 1, global: 1});
 }
 
-var debug = true;
+var debug = false;
 
 describe('ngdeploy', function () {
     var sandbox;
     var mockApi;
 
-    beforeEach(function(){
+    beforeEach(function () {
         sandbox = sinon.sandbox.create();
         helpers.mockAuth(sandbox);
         mockApi = sandbox.mock(api);
         clearConfig();
 
-        if(!debug) {
-            api.logger.configure({'level':'error'});
+        if (!debug) {
+            api.logger.configure({'level': 'error'});
         }
     });
 
-    afterEach(function() {
+    afterEach(function () {
         sandbox.restore();
     });
 
-    describe('.addDomain()', function(){
-        before(function(){
-            ngdeploy.login({global:1, accountToken:"global token"});
+    describe('.addDomain()', function () {
+        before(function () {
+            ngdeploy.login({global: 1, accountToken: "global token"});
         });
 
-        context('requires an domain, id, and access token', function(){
-            it('succeeds with all', function(){
-                return expect(domains.add({appId:1,domain:'google.com'})).to.not.be.an("error");
+        context('requires an domain, id, and access token', function () {
+            it('succeeds with all', function () {
+                return expect(domains.add({appId: 1, domain: 'google.com'})).to.not.be.an("error");
             });
         });
     });
 
-          describe('.login()', function(){
-            it('sets the global access key', function(){
-                ngdeploy.login({global:1, accountToken:"global token"});
-                console.log(config.findToken());
-                assert.equal(config.findToken(), "global token");
-            });
+    describe('.login()', function () {
+        it('sets the global access key', function () {
+            ngdeploy.login({global: 1, accountToken: "global token"});
+            assert.equal(config.findToken(), "global token");
+        });
 
-            it('sets the local access key', function(){
-                ngdeploy.login({global:0, accountToken:"local token"});
-                assert.equal(config.findToken(), "local token");
-            });
+        it('sets the local access key', function () {
+            ngdeploy.login({global: 0, accountToken: "local token"});
+            assert.equal(config.findToken(), "local token");
+        });
 
-          });
+    });
 
-         describe('.init()', function () {
+    describe('.init()', function () {
 
-            it('should not create an empty .ngdeploy file', function () {
-                try{
-                    ngdeploy.init();
-                }catch(e){}
-                assert.equal(fs.existsSync('.ngdeploy'), false);
-              });
-          });
+        it('should not create an empty .ngdeploy file', function () {
+            try {
+                ngdeploy.init();
+            } catch (e) {
+            }
+            assert.equal(fs.existsSync('.ngdeploy'), false);
+        });
+    });
 
-        describe('.init(appname,./dist)', function () {
+    describe('.init(appname,./dist)', function () {
 
-            it('should create an .ngdeploy file', function () {
-                var testJson;
+        it('should create an .ngdeploy file', function () {
+            var testJson;
 
-                try{
-                    ngdeploy.createConfiguration(test);
-                }catch(e){}
+            try {
+                ngdeploy.createConfiguration(test);
+            } catch (e) {
+            }
 
-                assert.equal(fs.existsSync('.ngdeploy'), true);
+            assert.equal(fs.existsSync('.ngdeploy'), true);
 
-                try {
-                     testJson = JSON.parse(fs.readFileSync('.ngdeploy'));
-                }catch(e){}
-                assert.equal(testJson.name, 'testapp');
-                assert.equal(testJson.dist, './dist');
-                assert.equal(testJson.id, 11);
+            try {
+                testJson = JSON.parse(fs.readFileSync('.ngdeploy'));
+            } catch (e) {
+            }
+            assert.equal(testJson.name, 'testapp');
+            assert.equal(testJson.dist, './dist');
+            assert.equal(testJson.id, 11);
 
-              });
+        });
 
-          });
+    });
 
-        describe('.clean()', function () {
-            before(function () {
-                try{
-                    ngdeploy.createConfiguration(test);
-                }catch(e){}
-              });
+    describe('.clean()', function () {
+        before(function () {
+            try {
+                ngdeploy.createConfiguration(test);
+            } catch (e) {
+            }
+        });
 
-            it('(default) should remove the .ngdeploy file', function () {
-                ngdeploy.clean();
-                assert.equal(fs.existsSync('.ngdeploy'), false);
-              });
+        it('(default) should remove the .ngdeploy file', function () {
+            ngdeploy.clean();
+            assert.equal(fs.existsSync('.ngdeploy'), false);
+        });
 
-            it(' -g should remove the ~/.ngdeploy file', function () {
-                config.setAccountToken("test account",1);
-                ngdeploy.clean({global:1});
-                assert.equal(fs.existsSync('.ngdeploy'), false);
-            });
+        it(' -g should remove the ~/.ngdeploy file', function () {
+            config.setAccountToken("test account", 1);
+            ngdeploy.clean({global: 1});
+            assert.equal(fs.existsSync('.ngdeploy'), false);
+        });
 
-            it(' -l should remove the .ngdeploy file', function(){
-                    config.setAccountToken("test account",0);
-                    ngdeploy.clean({local:1});
-                    assert.equal(fs.existsSync('.ngdeploy'), false);
-            });
-            it(' -l -g should remove both .ngdeploy files', function(){
-                config.setAccountToken("local account",0);
-                config.setAccountToken("global account",1);
-                ngdeploy.clean({local:1, global:1});
-                assert.equal(fs.existsSync('.ngdeploy'), false);
-                assert.equal(fs.existsSync( config.getUserHome() + '/.ngdeploy'), false);
-            });
-          });
+        it(' -l should remove the .ngdeploy file', function () {
+            config.setAccountToken("test account", 0);
+            ngdeploy.clean({local: 1});
+            assert.equal(fs.existsSync('.ngdeploy'), false);
+        });
+        it(' -l -g should remove both .ngdeploy files', function () {
+            config.setAccountToken("local account", 0);
+            config.setAccountToken("global account", 1);
+            ngdeploy.clean({local: 1, global: 1});
+            assert.equal(fs.existsSync('.ngdeploy'), false);
+            assert.equal(fs.existsSync(config.getUserHome() + '/.ngdeploy'), false);
+        });
+    });
 
 
     describe('init with local and global accessToken', function () {
@@ -144,32 +147,32 @@ describe('ngdeploy', function () {
         });
     });
 
-    describe("end to end test", function(){
-        it("log the user in", function(){
+    describe("end to end test", function () {
+        it("log the user in", function () {
             //ngdeploy.login('test key');
         });
 
-        it("create an application", function(){
+        it("create an application", function () {
             //ngdeploy.create(test);
         });
 
-        it("initialize the test application", function(){
+        it("initialize the test application", function () {
             //ngdeploy.init(test);
         });
 
-        it("push it to development", function(){
+        it("push it to development", function () {
             //ngdeploy.push();
         });
 
-        it("promote to staging", function(){
+        it("promote to staging", function () {
             //ngdeploy.promote('staging');
         });
 
-        it("promote to production", function(){
+        it("promote to production", function () {
             //ngdeploy.promote('production');
         });
 
-        it("add a domain", function(){
+        it("add a domain", function () {
             //domains.add('http://ngdeploy.org');
         });
     });
